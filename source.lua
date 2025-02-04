c22c26f4b5c8b795428f4639b61022b3a814a18e
Ollie0000 <132388201+Ollie0000@users.noreply.github.com>
2025-02-04 22:43:59 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,1 +1,11 @@
+local Games = {
+    [527730528] = 'https://raw.githubusercontent.com/Velora-Softworks-LTD/Velora/refs/heads/main/magic%20training.lua'
+}
 
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+if Games[game.PlaceId] then
+    pcall(loadstring(game.HttpGet, game, Games[game.PlaceId]))
+end
